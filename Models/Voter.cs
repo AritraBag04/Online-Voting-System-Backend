7d95bbcac4601303baf0ae6a1c602517d09959b4
Liquidator04 <aritrabag04@gmail.com>
2025-02-21 11:03:54 +0530

    Initial Commit

---
+++ Models/Voter.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Voting_System_Backend.Models
+{
+    public class Voter
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public required byte[] Image { get; set; }
+    }
+}
---
+++ Models/VoterContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Online_Voting_System_Backend.Models
+{
+    public class VoterContext: DbContext
+    {
+        public VoterContext(DbContextOptions<VoterContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Voter> TodoItems { get; set; } = null!;
+    }
+}
