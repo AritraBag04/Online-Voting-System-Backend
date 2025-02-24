432a987057d8600d831a9226b2518b4e66bcdfa7
Liquidator04 <aritrabag04@gmail.com>
2025-02-24 22:22:34 +0530

    Created the first post request

--- Controllers/VoterController.cs
+++ Controllers/VoterController.cs
@@ -33,5 +33,26 @@ namespace Online_Voting_System_Backend.Controllers
 
             return File(voter.Image, "image/png");
         }
+        [HttpPost]
+        public async Task<ActionResult<Voter>> CreateVoter([FromForm] string name, [FromForm] IFormFile image)
+        {
+            if (string.IsNullOrEmpty(name) || image == null)
+                return BadRequest("Name and image are required.");
+            using var memoryStream = new MemoryStream();
+            await image.CopyToAsync(memoryStream);
+
+            var voter = new Voter
+            {
+                Name = name,
+                Image = memoryStream.ToArray() // Store as byte array in the database
+            };
+
+            _context.Voters.Add(voter);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetVoters),
+                new { id = voter.Id }, voter
+            );
+        }
     }
 }
