758f801275872609f1258d7f24850782004a4759
Liquidator04 <aritrabag04@gmail.com>
2025-03-03 12:37:28 +0530

    Admin can create an election

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Voting_System_Backend.Models;
+
+namespace Online_Voting_System_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly VotingDbContext _context;
+        public AdminController(VotingDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Election>> GetAdminById(Guid id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null) return NotFound();
+            return Ok(admin);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Voter>> CreateAdmin()
+        {
+            var admin = new Admin
+            {
+
+            };
+            await _context.Admins.AddAsync(admin);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetAdminById),
+                new { Id = admin.Id},
+                admin
+            );
+        }
+    }
+}
---
+++ Controllers/ElectionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Voting_System_Backend.Models;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Online_Voting_System_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ElectionController : ControllerBase
+    {
+        private readonly VotingDbContext _context;
+        public ElectionController(VotingDbContext context) 
+        {
+            _context = context; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Election>> GetElectionById(Guid id)
+        {
+            var election = await _context.Elections.FindAsync(id);
+            if (election == null) return NotFound();
+            return Ok(election);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Voter>> CreateElection([FromForm] Guid admin_id, [FromForm] DateTime start_date, [FromForm] DateTime end_date)
+        {
+            if (admin_id == Guid.Empty || start_date == default || end_date == default)
+                return BadRequest("Name and image are required.");
+            var election = new Election
+            {
+                AdminId = admin_id,
+                StartDate = start_date,
+                EndDate = end_date
+            };
+            await _context.Elections.AddAsync(election);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetElectionById), 
+                new { id = election.Id },
+                election
+            );
+
+        }
+    }
+}
